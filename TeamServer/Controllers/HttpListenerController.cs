780513fd5388d66539978aea126a0760891d5318
dead-jaskier <symon3@protonmail.com>
2021-11-24 23:59:22 -0600

    Created TeamServer project

---
+++ TeamServer/Controllers/HttpListenerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TeamServer.Controllers
+{
+    [Controller]
+    public class HttpListenerController : ControllerBase
+    {
+        public IActionResult HandleImplant()
+        {
+            return Ok("Your Listener works");
+        }
+    }
+}
---
+++ TeamServer/Controllers/ListenersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TeamServer.Services;
+
+namespace TeamServer.Controllers
+{
+    [ApiController]
+    public class ListenersController : ControllerBase
+    {
+        private readonly IListenerService _listeners;
+
+        public ListenersController(IListenerService listeners)
+        {
+            _listeners = listeners;
+        }
+
+        [HttpGet]
+        public IActionResult GetListeners()
+        {
+            var listeners = _listeners.GetListeners();
+            return Ok(listeners);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetListener(string name)
+        {
+            var listener = _listeners.GetListener(name);
+            if (listener == null) return NotFound();
+
+            return Ok(listener);
+        }
+    }
+}
